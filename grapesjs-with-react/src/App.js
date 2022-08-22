ce5a579601f0af073ea5c839f5f7ad08cad6fb8b
pamplonapaulo <25485709+pamplonapaulo@users.noreply.github.com>
2022-08-22 15:29:07 -0300

    grapesjs up and running

---
+++ grapesjs-with-react/src/App.js
@@ -0,0 +1,12 @@
+import WebBuilder from './components/webBuilder'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <WebBuilder/>
+    </div>
+  );
+}
+
+export default App
